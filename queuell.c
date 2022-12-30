9a45df83a5e3bea1e8da639b5426ddbe80a77a2e
krishn-maloo <98144812+krishn-maloo@users.noreply.github.com>
2022-12-30 14:45:01 +0530

    Add files via upload

---
+++ queuell.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node
+{
+	int value;
+	struct node *next;
+};
+
+typedef struct node *NODE;
+
+NODE getnode()
+{
+	NODE temp;
+	temp=(NODE)malloc(sizeof(struct node));
+	if (temp==NULL)
+	{
+		printf("Memory not allocated\n");
+		return NULL;
+	}
+	
+	return temp;
+}
+
+NODE insert_end(int item,NODE first)
+{
+	NODE new,last;
+    new=getnode();
+	new->value=item;
+	new->next=NULL;
+	if (first==NULL)
+	{
+		return new;
+	}
+	if(first->next==NULL)	
+	{
+		first->next=new;
+		return first;
+	}
+	last=first;
+	while(last->next!=NULL)
+		last=last->next;
+	last->next=new;
+	return first;
+}
+
+
+
+
+NODE delete_beg(NODE first)
+{
+    NODE temp;
+    if(first==NULL)
+    {
+        printf("Cannot delete\n");
+        return NULL;
+    }
+    temp=first;
+    temp=temp->next;
+    printf("Item deleted=%d",first->value);
+    free(first);
+    return temp;
+}
+
+void display(NODE first)
+{
+	NODE temp;
+	temp=first;
+	while(temp!=NULL)
+	{
+		printf("value stored in node=%d\n",temp->value);
+		temp=temp->next;
+	}
+}
+
+void main()
+{
+	NODE first=NULL;
+	int choice,pos,item;
+	while(1)
+	{
+		printf("\n1.Insert \n2.delete \n 3.Display\n");
+		printf("\n enter your choice\n");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+
+            case 1:printf("\nEnter the value to be inserted\n");
+			       scanf("%d",&item);
+			       first=insert_end(item,first);
+                   break;
+                   
+			    
+			case 2:first=delete_beg(first);
+                   break;
+
+			case 3:display(first);
+			       break;
+			         
+		    default:exit(0);
+			        
+		}
+		
+	}
+}
